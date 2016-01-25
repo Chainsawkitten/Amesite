191ea5435454e9d291454b8fe9123ec1b664d256
Albin Bernhardsson <catscratchesp@gmail.com>
2016-01-25 12:13:05 +0100

    Empty window

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello world" << endl;
-
+    if (!glfwInit())
+        return 1;
+    
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Modership", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        /// @todo Print error to log.
+    }
+    glfwMakeContextCurrent(window);
+    
+    while (glfwWindowShouldClose(window) == GL_FALSE) {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
     return 0;
 }
