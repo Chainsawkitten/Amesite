ff5c781397f318575d576ae789d8612b0668aa0e
Ivar Björling <ivarbjorling@hotmail.com>
2016-01-27 12:26:19 +0200

    Updated the component handling with a template interface

--- src/Engine/Entity/Entity.hpp
+++ src/Engine/Entity/Entity.hpp
@@ -6,14 +6,33 @@ namespace Component {
 }
 
 class Entity {
-        public:
-            Entity();
-            ~Entity();
+    public:
+        /// Create a new Entity.
+        Entity();
 
-            void CreateLens();
+        /// Destructor.
+        ~Entity();
 
-            void CreateTransform();
+        /// Add Component to Entity
+        /**
+        * @return Return new Component or nullptr if Entity allready contains the Component.
+        */
+        template<typename T> T* AddComponent() { return nullptr; }
+        template<> Component::Lens* AddComponent<Component::Lens>() { return CreateLens(); }
+        template<> Component::Transform* AddComponent<Component::Transform>() { return CreateTransform(); }
 
-            Component::Lens* mLens;
-            Component::Transform* mTransform;
+        /// Get Component of Entity
+        /**
+        * @return Return Component or nullptr if Entity doesn't contains the Component.
+        */
+        template<typename T> T* GetComponent() { return nullptr; }
+        template<> Component::Lens* GetComponent<Component::Lens>() { return mLens; }
+        template<> Component::Transform* GetComponent<Component::Transform>() { return mTransform; }
+
+    private:
+        Component::Lens* CreateLens();
+        Component::Transform* CreateTransform();
+
+        Component::Lens* mLens;
+        Component::Transform* mTransform;
 };
