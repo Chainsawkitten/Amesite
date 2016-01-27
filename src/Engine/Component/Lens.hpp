bb5ef0d8e754bd38cd16d2c7dfa89a9afb729d37
Albin Bernhardsson <catscratchesp@gmail.com>
2016-01-27 12:50:44 +0100

    Fix component documentation.

--- src/Engine/Component/Lens.hpp
+++ src/Engine/Component/Lens.hpp
@@ -1,25 +1,41 @@
 #pragma once
+
 #include <glm\glm.hpp>
 
 namespace Component {
 
-    ///Component handling camera
+    /// Component handling a camera lens.
     class Lens {
         public:
-            ///Create a new lens
+            /// Create a new lens.
             Lens();
-            ///Destructor
+            
+            /// Destructor.
             ~Lens();
 
-            ///Create projection matrix
+            /// Get the projection matrix.
             /**
-            *@param vec2 screen size in pixels
-            *@return mat4 containing projection matrix
-            */
+             * @param screenSize Screen size in pixels.
+             * @return The projection matrix.
+             */
             glm::mat4 GetProjection(const glm::vec2& screenSize) const;
-
+            
+            /// Field of view, in degrees.
+            /**
+             * Default: 45.0
+             */
             float mFieldOfView;
+            
+            /// Near plane.
+            /**
+             * Default: 0.5
+             */
             float mZNear;
+            
+            /// Far plane.
+            /**
+             * Default: 100.0
+             */
             float mZFar;
     };
 
--- src/Engine/Component/Transform.hpp
+++ src/Engine/Component/Transform.hpp
@@ -4,49 +4,59 @@
 
 namespace Component{
 
-    /// Component providing transformation
+    /// Component providing transformation.
     class Transform {
     public:
-        ///Create new transform
+        /// Create new transform
         Transform();
 
-        ///Destructor
+        /// Destructor
         ~Transform();
 
         /// Get model matrix.
         /**
-        * @return The object's model matrix, containing translation, scaling and rotation.
-        */
+         * @return The object's model matrix, containing translation, scaling and rotation.
+         */
         glm::mat4 GetOrientation() const;
 
         /// Set rotation by an offset.
         /**
-        * Rotate the object relative to its current rotation.
-        * @param horizontalAngle Relative horizontal rotation (in degrees).
-        * @param verticalAngle Relative vertical rotation (in degrees).
-        * @param tiltAngle Relative tilt rotation (in degrees).
-        */
+         * Rotate the object relative to its current rotation.
+         * @param horizontalAngle Relative horizontal rotation (in degrees).
+         * @param verticalAngle Relative vertical rotation (in degrees).
+         * @param tiltAngle Relative tilt rotation (in degrees).
+         */
         void Rotate(float horizontalAngle, float verticalAngle, float tiltAngle);
 
         /// Move the current position by an offset.
         /**
-        * Move the object relative to its current position.
-        * @param offset Offset by which to move by.
-        */
+         * Move the object relative to its current position.
+         * @param offset Offset by which to move by.
+         */
         void Move(const glm::vec3 &offset);
 
         /// Move the current position by an offset in x,y,z.
         /**
-        * Move the object relative to its current position.
-        * @param x offset, y offset, z offset.
-        */
+         * Move the object relative to its current position.
+         * @param x Offset along x-axis.
+         * @param y Offset along y-axis.
+         * @param z Offset along z-axis.
+         */
         void Move(float x, float y, float z);
-
+        
+        /// Position in the world.
         glm::vec3 mPosition;
+        
+        /// Scale.
         glm::vec3 mScale;
-
+        
+        /// Yaw (in degrees).
         float mHorizontalAngle;
+        
+        /// Pitch (in degrees).
         float mVerticalAngle;
+        
+        /// Roll (in degrees).
         float mTiltAngle;
 
     };
