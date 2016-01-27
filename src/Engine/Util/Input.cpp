bb0e97cd55cfbbdf8bf6545a35c3780e0a4a12d4
splusharn <splusharn@gmail.com>
2016-01-27 15:26:24 +0100

    Fixed proposed changes in pull request

--- src/Engine/Util/Input.cpp
+++ src/Engine/Util/Input.cpp
@@ -5,108 +5,110 @@
 std::map<GLFWwindow*, InputHandler*> inputMap;
 
 void characterCallback(GLFWwindow* window, unsigned int codePoint) {
-	inputMap[window]->CharacterCallback(codePoint);
+    inputMap[window]->CharacterCallback(codePoint);
 }
 
 void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
-	inputMap[window]->ScrollCallback(yoffset);
+    inputMap[window]->ScrollCallback(yoffset);
 }
 
-InputHandler* InputHandler::activeInstance = nullptr;
+InputHandler* InputHandler::mActiveInstance = nullptr;
 
 InputHandler::InputHandler(GLFWwindow* window) {
-	this->window = window;
-	inputMap[window] = this;
+    this->mWindow = window;
+    inputMap[window] = this;
 
-	// Init mouse state.
-	for (int i = 0; i<3; i++) {
-		mouseState[i] = false;
-		mouseStateLast[i] = false;
-	}
-	glfwSetScrollCallback(window, scrollCallback);
-	lastScroll = 0.0;
-	scroll = 0.0;
+    // Init mouse state.
+    for (int i = 0; i<3; i++) {
+        mMouseState[i] = false;
+        mMouseStateLast[i] = false;
+    }
+    glfwSetScrollCallback(window, scrollCallback);
+    mLastScroll = 0.0;
+    mScroll = 0.0;
 
     // Init button states.
     for (int player = 0; player < PLAYERS; player++) {
         for (int button = 0; button < BUTTONS; button++) {
-            buttonReleased[player][button] = true;
-            buttonTriggered[player][button] = false;
-            buttonValue[player][button] = 0.0;
-            joystickAxis[player][button] = false;
+            mButtonReleased[player][button] = true;
+            mButtonTriggered[player][button] = false;
+            mButtonValue[player][button] = 0.0;
+            mJoystickAxis[player][button] = false;
         }
     }
 
-    bindings = new std::vector<int>[PLAYERS*BUTTONS];
+    mBindings = new std::vector<int>[PLAYERS*BUTTONS];
 
-	glfwSetCharCallback(window, characterCallback);
-	text = "";
-	tempText = "";
+    glfwSetCharCallback(window, characterCallback);
+    mText = "";
+    mTempText = "";
 }
 
 InputHandler::~InputHandler() {
-    delete[] bindings;
+    delete[] mBindings;
 }
 
 InputHandler* InputHandler::GetActiveInstance() {
-	return activeInstance;
+    return mActiveInstance;
 }
 
 void InputHandler::SetActive() {
-	activeInstance = this;
+    mActiveInstance = this;
 }
 
 void InputHandler::Update() {
-	// Update mouse.
-	for (int i = 0; i<3; i++) {
-		mouseStateLast[i] = mouseState[i];
-		mouseState[i] = (glfwGetMouseButton(window, i) == GLFW_PRESS);
-	}
-	lastScroll = scroll;
-	scroll = 0.0;
+    // Update mouse.
+    for (int i = 0; i<3; i++) {
+        mMouseStateLast[i] = mMouseState[i];
+        mMouseState[i] = (glfwGetMouseButton(mWindow, i) == GLFW_PRESS);
+    }
+    mLastScroll = mScroll;
+    mScroll = 0.0;
 
-	glfwGetCursorPos(window, &cursorX, &cursorY);
+    glfwGetCursorPos(mWindow, &mCursorX, &mCursorY);
 
     // Update joystick axis.
     int axisOneCount = 0;
-    joystickAxisData[PLAYER_ONE] = glfwGetJoystickAxes(PLAYER_ONE, &axisOneCount);
+    mJoystickAxisData[PLAYER_ONE] = glfwGetJoystickAxes(PLAYER_ONE, &axisOneCount);
     //Log() << axisOneCount;
 
     // Update joystick buttons.
     int buttonOneCount = 0;
-    joystickButtonPressed[PLAYER_ONE] = glfwGetJoystickButtons(PLAYER_ONE, &buttonOneCount);
+    mJoystickButtonPressed[PLAYER_ONE] = glfwGetJoystickButtons(PLAYER_ONE, &buttonOneCount);
     //Log() << buttonOneCount;
 
     // Update joystick axis.
     int axisTwoCount = 0;
-    joystickAxisData[PLAYER_TWO] = glfwGetJoystickAxes(PLAYER_TWO, &axisTwoCount);
+    mJoystickAxisData[PLAYER_TWO] = glfwGetJoystickAxes(PLAYER_TWO, &axisTwoCount);
     //Log() << axisTwoCount;
 
     // Update joystick buttons.
     int buttonTwoCount = 0;
-    joystickButtonPressed[PLAYER_ONE] = glfwGetJoystickButtons(PLAYER_TWO, &buttonTwoCount);
+    mJoystickButtonPressed[PLAYER_ONE] = glfwGetJoystickButtons(PLAYER_TWO, &buttonTwoCount);
     //Log() << buttonTwoCount;
 
-	// Update button states depending on bindings.
-    for (int player = 0; player < (PLAYERS-1); player++) {
+    // Update button states depending on bindings.
+    for (int player = 0; player < (PLAYERS - 1); player++) {
         for (int button = 0; button < BUTTONS; button++) {
             double value = 0.0;
-            for (auto &key : bindings[player*BUTTONS + button]) {
+            for (auto &key : mBindings[player*BUTTONS + button]) {
 
                 // Switch the different input devices.
-                switch (bindingDevice[player][button]) {
+                switch (mBindingDevice[player][button]) {
                 case KEYBOARD:
-                    if (glfwGetKey(window, key) == GLFW_PRESS)
+                    if (glfwGetKey(mWindow, key) == GLFW_PRESS)
                         value = 1.0;
                 case JOYSTICK:
                     // Scalar axis of joystick.
-                    if (joystickAxis[player][button]) {
-                        value = joystickAxisData[player][key];
-                    // Buttons of joystick.
-                    } else {
-                        if (joystickButtonPressed[player][button] == GLFW_PRESS) {
+                    if (mJoystickAxis[player][button]) {
+                        value = mJoystickAxisData[player][key];
+                        // Buttons of joystick.
+                    }
+                    else {
+                        if (mJoystickButtonPressed[player][button] == GLFW_PRESS) {
                             value = 1.0;
-                        } else {
+                        }
+                        else {
                             value = 0.0;
                         }
                     }
@@ -115,109 +117,109 @@ void InputHandler::Update() {
                 }
 
             }
-            buttonTriggered[player][button] = (buttonValue[player][button] == 1.0) && (value == 1.0);
-            buttonReleased[player][button] = (buttonValue[player][button] == 0.0) && (value == 0.0);
-            buttonValue[player][button] = value;           
+            mButtonTriggered[player][button] = (mButtonValue[player][button] == 1.0) && (value == 1.0);
+            mButtonReleased[player][button] = (mButtonValue[player][button] == 0.0) && (value == 0.0);
+            mButtonValue[player][button] = value;
         }
     }
     // Update the 'Anyone' input section
     for (int button = 0; button < BUTTONS; button++) {
-        buttonTriggered[ANYONE][button] = buttonTriggered[PLAYER_ONE][button] || buttonTriggered[PLAYER_TWO][button];
-        buttonReleased[ANYONE][button] = buttonReleased[PLAYER_ONE][button] || buttonReleased[PLAYER_TWO][button];
-        if (buttonValue[PLAYER_ONE][button] == 1.0 || buttonValue[PLAYER_ONE][button] == 1.0) {
-            buttonValue[ANYONE][button] = 1.0;
+        mButtonTriggered[ANYONE][button] = mButtonTriggered[PLAYER_ONE][button] || mButtonTriggered[PLAYER_TWO][button];
+        mButtonReleased[ANYONE][button] = mButtonReleased[PLAYER_ONE][button] || mButtonReleased[PLAYER_TWO][button];
+        if (mButtonValue[PLAYER_ONE][button] == 1.0 || mButtonValue[PLAYER_ONE][button] == 1.0) {
+            mButtonValue[ANYONE][button] = 1.0;
         }
         else {
-            buttonValue[ANYONE][button] = 0.0;
+            mButtonValue[ANYONE][button] = 0.0;
         }
     }
 
-	// Update text.
-	text = tempText;
-	tempText = "";
+    // Update text.
+    mText = mTempText;
+    mTempText = "";
 }
 
 bool InputHandler::MousePressed(int button) const {
-	return mouseState[button] && !mouseStateLast[button];
+    return mMouseState[button] && !mMouseStateLast[button];
 }
 
 bool InputHandler::MouseDown(int button) const {
-	return mouseState[button];
+    return mMouseState[button];
 }
 
 bool InputHandler::MouseReleased(int button) const {
-	return !mouseState[button] && mouseStateLast[button];
+    return !mMouseState[button] && mMouseStateLast[button];
 }
 
 bool InputHandler::ScrollUp() const {
-	return lastScroll > 0.0;
+    return mLastScroll > 0.0;
 }
 
 bool InputHandler::ScrollDown() const {
-	return lastScroll < 0.0;
+    return mLastScroll < 0.0;
 }
 
 double InputHandler::CursorX() const {
-	return cursorX;
+    return mCursorX;
 }
 
 double InputHandler::CursorY() const {
-	return cursorY;
+    return mCursorY;
 }
 
 void InputHandler::CenterCursor() {
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glfwSetCursorPos(window, width / 2, height / 2);
+    int width, height;
+    glfwGetFramebufferSize(mWindow, &width, &height);
+    glfwSetCursorPos(mWindow, width / 2, height / 2);
 
-	cursorX = static_cast<double>(width / 2);
-	cursorY = static_cast<double>(height / 2);
+    mCursorX = static_cast<double>(width / 2);
+    mCursorY = static_cast<double>(height / 2);
 }
 void InputHandler::AssignJoystick(Button button, bool axis, int index, Player player) {
-    bindings[BUTTONS*player + button].push_back(index);
-    joystickAxis[player][button]= axis;
-    bindingDevice[player][button] = JOYSTICK;
+    mBindings[BUTTONS*player + button].push_back(index);
+    mJoystickAxis[player][button] = axis;
+    mBindingDevice[player][button] = JOYSTICK;
 }
 
 double InputHandler::ButtonValue(Button button, Player player) const {
-    return buttonValue[player][button];
+    return mButtonValue[player][button];
 }
 
 void InputHandler::AssignKeyboard(Button button, int key, Player player) {
-    bindings[BUTTONS*player + button].push_back(key);
-    bindingDevice[player][button] = KEYBOARD;
+    mBindings[BUTTONS*player + button].push_back(key);
+    mBindingDevice[player][button] = KEYBOARD;
 }
 
 bool InputHandler::Pressed(Button button, Player player) {
-    if (joystickAxis[player][button])
+    if (mJoystickAxis[player][button])
         Log() << "You are checking if a scalar axis is pressed or not - this will almost never be true.";
-	return buttonValue[player][button] == 1.0;
+    return mButtonValue[player][button] == 1.0;
 }
 
 bool InputHandler::Triggered(Button button, Player player) {
-    if (joystickAxis[player][button])
+    if (mJoystickAxis[player][button])
         Log() << "You are checking if a scalar axis is triggered or not - this will almost never be true.";
-	return buttonTriggered[player][button];
+    return mButtonTriggered[player][button];
 }
 
 bool InputHandler::Released(Button button, Player player) {
-    if (joystickAxis[player][button])
+    if (mJoystickAxis[player][button])
         Log() << "You are checking if a scalar axis is released or not - this is almost always true.";
-	return buttonReleased[player][button];
+    return mButtonReleased[player][button];
 }
 
 const std::string& InputHandler::Text() const {
-	return text;
+    return mText;
 }
 
 void InputHandler::CharacterCallback(unsigned int codePoint) {
-	tempText += static_cast<char>(codePoint);
+    mTempText += static_cast<char>(codePoint);
 }
 
 void InputHandler::ScrollCallback(double yoffset) {
-	scroll += yoffset;
+    mScroll += yoffset;
 }
 
 InputHandler* Input() {
-	return InputHandler::GetActiveInstance();
+    return InputHandler::GetActiveInstance();
 }
