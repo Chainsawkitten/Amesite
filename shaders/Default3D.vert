28593e14cded6d3d5859b007a04003a24b66850e
NinjaDanz3r <danielpejcinovski@hotmail.com>
2016-01-27 10:53:59 +0100

    Work on shader and projection

--- shaders/Default3D.vert
+++ shaders/Default3D.vert
@@ -12,5 +12,5 @@ uniform mat4 model;
 uniform mat4 view;
 
 void main () {
-    gl_Position = MVP*vec4(vertexPosition, 1.0);
+    gl_Position = model*view*projection*vec4(vertexPosition, 1.0);
 }
--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,10 +1,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
 #include <MainWindow.hpp>
 
-#include <Engine\Geometry\Cube.hpp>
-#include <Engine\Shader\Shader.hpp>
-#include <Engine\Shader\ShaderProgram.hpp>
+#include <Engine/Geometry/Cube.hpp>
+#include <Engine/Shader/Shader.hpp>
+#include <Engine/Shader/ShaderProgram.hpp>
 
 #include "Default3D.frag.hpp"
 #include "Default3D.vert.hpp"
@@ -13,9 +14,9 @@
 #include "Util/GameSettings.hpp"
 #include <Util/FileSystem.hpp>
 
-#include <Engine\Entity\Entity.hpp>
-#include <Engine\Component\Transform.hpp>
-#include <Engine\Component\Lens.hpp>
+#include <Engine/Entity/Entity.hpp>
+#include <Engine/Component/Transform.hpp>
+#include <Engine/Component/Lens.hpp>
 
 using namespace std;
 
@@ -34,6 +35,7 @@ int main() {
     window->Init();
 
     Geometry::Cube cubeDefenderOfThePolyverse;
+    Component::Transform cubeTransform;
     
     Shader* vertShader = new Shader(DEFAULT3D_VERT, DEFAULT3D_VERT_LENGTH, GL_VERTEX_SHADER);
     Shader* fragShader = new Shader(DEFAULT3D_FRAG, DEFAULT3D_FRAG_LENGTH, GL_FRAGMENT_SHADER);
@@ -44,16 +46,20 @@ int main() {
     testCamera.CreateTransform();
 
     testCamera.mTransform->Move(-1.f, 1.f, -1.f);
-    testCamera.mTransform->Rotate(-30.f, -20.f, 0.f);
+    testCamera.mTransform->Rotate(30.f, 20.f, 0.f);
 
     shaderProgram->Use();
 
-    //TODO: RENDERING PERSPECTIVE.
-    glUniformMatrix4fv(shaderProgram->GetUniformLocation("model"), 1, GL_FALSE, &model[0][0]);
-    glUniformMatrix4fv(shaderProgram->GetUniformLocation("view"), 1, GL_FALSE, &model[0][0]);
-    glUniformMatrix4fv(shaderProgram->GetUniformLocation("projection"), 1, GL_FALSE, &model[0][0]);
+    glm::mat4 model = glm::translate(glm::mat4(), cubeTransform.mPosition) * cubeTransform.GetOrientation() * glm::scale(glm::mat4(), cubeTransform.mScale);
+
+    glm::mat4 view = testCamera.mTransform->GetOrientation()*glm::translate(glm::mat4(),-testCamera.mTransform->mPosition);
+    glm::mat4 projection = testCamera.mLens->GetProjection(glm::vec2(GameSettings::GetInstance().GetLong("Screen Width"), GameSettings::GetInstance().GetLong("Screen Height")));
 
     glBindVertexArray(cubeDefenderOfThePolyverse.GetVertexArray());
+
+    glUniformMatrix4fv(shaderProgram->GetUniformLocation("model"), 1, GL_FALSE, &model[0][0]);
+    glUniformMatrix4fv(shaderProgram->GetUniformLocation("view"), 1, GL_FALSE, &view[0][0]);
+    glUniformMatrix4fv(shaderProgram->GetUniformLocation("projection"), 1, GL_FALSE, &projection[0][0]);
 
     while (!window->ShouldClose()) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
