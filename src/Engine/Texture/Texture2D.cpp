41af519d726da51fff6d72a3715065879ec77adf
Albin Bernhardsson <catscratchesp@gmail.com>
2016-01-28 16:06:32 +0100

    2D textures.

---
+++ src/Engine/Texture/Texture2D.cpp
@@ -0,0 +1,150 @@
+#include "Texture2D.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_JPEG
+#define STBI_ONLY_PNG
+#define STBI_ONLY_TGA
+#include <stb_image.h>
+#include "../Util/Log.hpp"
+#include "Default2D.vert.hpp"
+#include "Texture2D.frag.hpp"
+#include "../Resources.hpp"
+#include "../Shader/Shader.hpp"
+#include "../Shader/ShaderProgram.hpp"
+#include "../Geometry/Square.hpp"
+
+Texture2D::Texture2D(const char* filename) {
+    glGenTextures(1, &mTexID);
+    glBindTexture(GL_TEXTURE_2D, mTexID);
+    
+    // Load texture from file.
+    int components;
+    unsigned char* data = stbi_load(filename, &mWidth, &mHeight, &components, 0);
+    
+    if (data == NULL)
+        Log() << "Couldn't load image " << filename << "\n";
+    
+    // Give the image to OpenGL.
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mWidth, mHeight, 0, Format(components), GL_UNSIGNED_BYTE, data);
+    
+    stbi_image_free(data);
+    
+    // When MAGnifying the image (no bigger mipmap available), use LINEAR filtering.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // When MINifying the image, use a LINEAR blend of two mipmaps, each filtered LINEARLY too.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    
+    // Repeat texture when texture coordinates outside 0.0-1.0.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    // Generate mipmaps, by the way.
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    // For rendering.
+    mSquare = Resources().CreateSquare();
+    
+    mVertexShader = Resources().CreateShader(DEFAULT2D_VERT, DEFAULT2D_VERT_LENGTH, GL_VERTEX_SHADER);
+    mFragmentShader = Resources().CreateShader(TEXTURE2D_FRAG, TEXTURE2D_FRAG_LENGTH, GL_FRAGMENT_SHADER);
+    mShaderProgram = Resources().CreateShaderProgram({ mVertexShader, mFragmentShader });
+}
+
+Texture2D::Texture2D(const char *source, int sourceLength) {
+    glGenTextures(1, &mTexID);
+    glBindTexture(GL_TEXTURE_2D, mTexID);
+    
+    // Load texture from file.
+    int components;
+    unsigned char* data = stbi_load_from_memory(reinterpret_cast<const unsigned char*>(source), sourceLength, &mWidth, &mHeight, &components, 0);
+    
+    if (data == NULL)
+        Log() << "Couldn't load headerized image.\n";
+    
+    // Give the image to OpenGL.
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mWidth, mHeight, 0, Format(components), GL_UNSIGNED_BYTE, data);
+    
+    stbi_image_free(data);
+    
+    // When MAGnifying the image (no bigger mipmap available), use LINEAR filtering.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // When MINifying the image, use a LINEAR blend of two mipmaps, each filtered LINEARLY too.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    
+    // Repeat texture when texture coordinates outside 0.0-1.0.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    // Generate mipmaps, by the way.
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    // For rendering.
+    mSquare = Resources().CreateSquare();
+    
+    mVertexShader = Resources().CreateShader(DEFAULT2D_VERT, DEFAULT2D_VERT_LENGTH, GL_VERTEX_SHADER);
+    mFragmentShader = Resources().CreateShader(TEXTURE2D_FRAG, TEXTURE2D_FRAG_LENGTH, GL_FRAGMENT_SHADER);
+    mShaderProgram = Resources().CreateShaderProgram({ mVertexShader, mFragmentShader });
+}
+
+Texture2D::~Texture2D() {
+    glDeleteTextures(1, &mTexID);
+    
+    Resources().FreeShader(mVertexShader);
+    Resources().FreeShader(mFragmentShader);
+    Resources().FreeShaderProgram(mShaderProgram);
+    
+    Resources().FreeSquare();
+}
+
+GLuint Texture2D::GetTextureID() const {
+    return mTexID;
+}
+
+int Texture2D::GetWidth() const {
+    return mWidth;
+}
+
+int Texture2D::GetHeight() const {
+    return mHeight;
+}
+
+void Texture2D::SetWrapping(GLint wrapMode) {
+    glBindTexture(GL_TEXTURE_2D, mTexID);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
+}
+
+void Texture2D::Render(const glm::vec2 &position, const glm::vec2 &size, const glm::vec2& screenSize) const {
+    // Disable depth testing.
+    GLboolean depthTest = glIsEnabled(GL_DEPTH_TEST);
+    glDisable(GL_DEPTH_TEST);
+    
+    // Blending enabled.
+    GLboolean blend = glIsEnabled(GL_BLEND);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    mShaderProgram->Use();
+    
+    // Texture unit 0 is for base images.
+    glUniform1i(mShaderProgram->GetUniformLocation("baseImage"), 0);
+    
+    // Base image texture
+    glActiveTexture(GL_TEXTURE0 + 0);
+    glBindTexture(GL_TEXTURE_2D, mTexID);
+    
+    // Set location and size.
+    glUniform2fv(mShaderProgram->GetUniformLocation("position"), 1, &(position / screenSize)[0]);
+    glUniform2fv(mShaderProgram->GetUniformLocation("size"), 1, &(size / screenSize)[0]);
+    
+    glBindVertexArray(mSquare->GetVertexArray());
+    
+    glDrawElements(GL_TRIANGLES, mSquare->GetIndexCount(), GL_UNSIGNED_INT, (void*)0);
+    
+    // Reset depth testing and blending.
+    if (depthTest)
+        glEnable(GL_DEPTH_TEST);
+    if (!blend)
+        glDisable(GL_BLEND);
+}
---
+++ src/Engine/Texture/Texture2D.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "Texture.hpp"
+#include <glm/glm.hpp>
+
+namespace Geometry {
+    class Square;
+}
+class Shader;
+class ShaderProgram;
+
+/// A two-dimensional texture.
+/**
+ * Used to load a 2D texture from an image file.
+ */
+class Texture2D : public Texture {
+    public:
+        /// Create new texture from the given image file.
+        /**
+         * Supported image formats: TGA.
+         * @param filename Filename (relative or absolute) of the image file.
+         */
+        Texture2D(const char* filename);
+        
+        /// Create new texture from given source string.
+        /**
+         * @param source Source string containing the image file.
+         * @param sourceLength Length of the source string.
+         */
+        Texture2D(const char* source, int sourceLength);
+        
+        /// Destructor
+        ~Texture2D();
+        
+        /// Get OpenGL texture ID.
+        /**
+         * Used when binding a texture before draw calls.
+         * @return The OpenGL texture identifier
+         */
+        GLuint GetTextureID() const;
+        
+        /// Get the width of the texture.
+        /**
+         * @return The width of the texture in texels
+         */
+        int GetWidth() const;
+        
+        /// Get the height of the texture.
+        /**
+         * @return The height of the texture in texels
+         */
+        int GetHeight() const;
+        
+        /// Set how coordinates outside 0..1 are handled.
+        /**
+         * Default: GL_REPEAT
+         * @param wrapMode One of GL_REPEAT, GL_MIRRORED_REPEAT, GL_CLAMP_TO_EDGE or GL_CLAMP_TO_BORDER.
+         */
+        void SetWrapping(GLint wrapMode);
+        
+        /// Render the texture to the screen.
+        /**
+         * @param position Position on the screen, in pixels.
+         * @param size Size in pixels.
+         * @param screenSize Size of the screen in pixels.
+         */
+        void Render(const glm::vec2& position, const glm::vec2& size, const glm::vec2& screenSize) const;
+        
+    private:
+        GLuint mTexID;
+        int mWidth, mHeight;
+        
+        Geometry::Square* mSquare;
+        
+        // Shaders
+        Shader* mVertexShader;
+        Shader* mFragmentShader;
+        ShaderProgram* mShaderProgram;
+};
