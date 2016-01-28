5c866a53b7b90b5cbf1ecd5a9c66120db5f88f31
Albin Bernhardsson <catscratchesp@gmail.com>
2016-01-28 12:00:46 +0100

    Fix SoundBuffer indentation.

--- src/Engine/Audio/SoundBuffer.cpp
+++ src/Engine/Audio/SoundBuffer.cpp
@@ -5,20 +5,20 @@
 using namespace Audio;
 
 SoundBuffer::SoundBuffer(SoundFile* soundFile) {
-	// Create audio buffer.
-	alGetError();
-	alGenBuffers((ALuint)1, &mBuffer);
-	SoundSystem::CheckError("Couldn't create buffers.");
-
-	// Set the buffer data.
-	alBufferData(mBuffer, soundFile->Format(), soundFile->Data(), soundFile->Size(), soundFile->SampleRate());
-	SoundSystem::CheckError("Couldn't set buffer data.");
+    // Create audio buffer.
+    alGetError();
+    alGenBuffers((ALuint)1, &mBuffer);
+    SoundSystem::CheckError("Couldn't create buffers.");
+    
+    // Set the buffer data.
+    alBufferData(mBuffer, soundFile->Format(), soundFile->Data(), soundFile->Size(), soundFile->SampleRate());
+    SoundSystem::CheckError("Couldn't set buffer data.");
 }
 
 SoundBuffer::~SoundBuffer() {
-	alDeleteBuffers(1, &mBuffer);
+    alDeleteBuffers(1, &mBuffer);
 }
 
 ALuint SoundBuffer::Buffer() const {
-	return mBuffer;
+    return mBuffer;
 }
