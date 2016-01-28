750e81c1c23c7641c3f52f3b8781beb885f0ba48
Albin Bernhardsson <catscratchesp@gmail.com>
2016-01-28 12:02:06 +0100

    Fix SoundSystem indentation.

--- src/Engine/Audio/SoundSystem.cpp
+++ src/Engine/Audio/SoundSystem.cpp
@@ -9,44 +9,44 @@ using namespace Audio;
 SoundSystem* SoundSystem::mInstance = nullptr;
 
 SoundSystem::SoundSystem() {
-	// Open default audio device.
-	mDevice = alcOpenDevice(nullptr);
-	if (!mDevice)
-		Log() << "Couldn't open default audio device.\n";
-
-	// Create audio context.
-	mContext = alcCreateContext(mDevice, nullptr);
-	if (!alcMakeContextCurrent(mContext))
-		Log() << "Couldn't create audio context.\n";
-
-	mListener = new Listener();
-	mInstance = this;
+    // Open default audio device.
+    mDevice = alcOpenDevice(nullptr);
+    if (!mDevice)
+        Log() << "Couldn't open default audio device.\n";
+    
+    // Create audio context.
+    mContext = alcCreateContext(mDevice, nullptr);
+    if (!alcMakeContextCurrent(mContext))
+        Log() << "Couldn't create audio context.\n";
+    
+    mListener = new Listener();
+    mInstance = this;
 }
 
 SoundSystem::~SoundSystem() {
-	delete mListener;
-
-	alcMakeContextCurrent(nullptr);
-	alcDestroyContext(mContext);
-	alcCloseDevice(mDevice);
+    delete mListener;
+    
+    alcMakeContextCurrent(nullptr);
+    alcDestroyContext(mContext);
+    alcCloseDevice(mDevice);
 }
 
 Listener* SoundSystem::GetListener() const {
-	return mListener;
+    return mListener;
 }
 
 SoundSystem* SoundSystem::GetInstance() {
-	return mInstance;
+    return mInstance;
 }
 
 void SoundSystem::CheckError(const char* message) {
-	ALenum error = alGetError();
-	if (error != AL_NO_ERROR) {
-		Log() << message << "\n";
-		if (error == AL_INVALID_NAME) Log() << "Invalid name\n";
-		if (error == AL_INVALID_ENUM) Log() << "Invalid enum\n";
-		if (error == AL_INVALID_VALUE) Log() << "Invalid value\n";
-		if (error == AL_INVALID_OPERATION) Log() << "Invalid operation\n";
-		if (error == AL_OUT_OF_MEMORY) Log() << "Out of memory like!\n";
-	}
+    ALenum error = alGetError();
+    if (error != AL_NO_ERROR) {
+        Log() << message << "\n";
+        if (error == AL_INVALID_NAME) Log() << "Invalid name\n";
+        if (error == AL_INVALID_ENUM) Log() << "Invalid enum\n";
+        if (error == AL_INVALID_VALUE) Log() << "Invalid value\n";
+        if (error == AL_INVALID_OPERATION) Log() << "Invalid operation\n";
+        if (error == AL_OUT_OF_MEMORY) Log() << "Out of memory like!\n";
+    }
 }
