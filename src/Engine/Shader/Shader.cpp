0afb46f1f3f833475bc7599a0e6a7361480fb7c2
Albin Bernhardsson <catscratchesp@gmail.com>
2016-02-03 13:33:44 +0100

    Log shader compilation errors.

--- src/Engine/Shader/Shader.cpp
+++ src/Engine/Shader/Shader.cpp
@@ -1,4 +1,6 @@
 #include "Shader.hpp"
+#include <vector>
+#include "../Util/Log.hpp"
 
 Shader::Shader(const char* source, int sourceLength, GLenum shaderType) {
 	mShaderType = shaderType;
@@ -8,6 +10,20 @@ Shader::Shader(const char* source, int sourceLength, GLenum shaderType) {
     GLint length = sourceLength;
 	glShaderSource(mShader, 1, &source, &length);
 	glCompileShader(mShader);
+    
+    // Check for shader compilation errors.
+    GLint isCompiled = 0;
+    glGetShaderiv(mShader, GL_COMPILE_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE) {
+        GLint maxLength = 0;
+        glGetShaderiv(mShader, GL_INFO_LOG_LENGTH, &maxLength);
+    
+        // The maxLength includes the NULL character
+        std::vector<GLchar> infoLog(maxLength);
+        glGetShaderInfoLog(mShader, maxLength, &maxLength, &infoLog[0]);
+        
+        Log() << std::string(infoLog.begin(), infoLog.end());
+    }
 }
 
 Shader::~Shader() {
