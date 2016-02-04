dc80570ec516fe6957ae577b0c0dcdd94ed9f5dd
NinjaDanz3r <danielpejcinovski@hotmail.com>
2016-02-04 13:12:31 +0100

    Camera system done.

--- src/Game/System/CameraSystem.hpp
+++ src/Game/System/CameraSystem.hpp
@@ -8,8 +8,17 @@ class Entity;
 namespace System {
     class CameraSystem {
         public:
+            ///Constructor
             CameraSystem();
+
+            ///Destructor
             ~CameraSystem();
+
+            ///Update camera system.
+            /**
+            * @param scene The scene to update.
+            * @param deltaTime Time since last frame (in seconds).
+            */
             void Update(Scene& scene, float deltaTime);
         private:
 
