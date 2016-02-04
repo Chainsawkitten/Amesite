e65b6b1e9760ae2ee9ab610ad7f2aad1d3e21441
NinjaDanz3r <danielpejcinovski@hotmail.com>
2016-02-04 19:03:12 +0100

    Work on coop spawning

--- src/Game/System/DamageSystem.cpp
+++ src/Game/System/DamageSystem.cpp
@@ -1,8 +1,11 @@
 #include "DamageSystem.hpp"
 #include <Engine/Scene/Scene.hpp>
 #include <Entity/Entity.hpp>
+#include <Component/Physics.hpp>
+#include "../Component/Health.hpp"
 #include "../Component/Health.hpp"
 #include "../Component/Damage.hpp"
+#include "../Component/Controller.hpp"
 #include <vector>
 
 using namespace System;
@@ -42,6 +45,16 @@ void DamageSystem::Update(Scene& scene) {
 
     for (auto health : scene.GetAll<Component::Health>()) {
         if (health->health < 0.f) {
+            // If the killed entity is a player, do other stuff.
+            if (health->entity->GetComponent<Component::Controller>() != nullptr) {
+                if(health->cooldown < 0.f){
+                    //TODO: Implement actual coop spawning
+                    health->entity->GetComponent<Component::Transform>()->Move(0.f, -0.5f, 0.f);
+                    health->entity->GetComponent<Component::Transform>()->scale = glm::vec3(0.5f, 0.5f, 0.5f);
+                    health->entity->GetComponent<Component::Physics>()->velocityDragFactor = 0.1f;
+                }
+            }
+                
             scene.RemoveEntity(health->entity);
         }
     }
