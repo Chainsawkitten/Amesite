9c94742159fed08da47011cea16d04f4326de940
splusharn <splusharn@gmail.com>
2016-02-05 16:16:44 +0100

    remove log message and fix camera bug.

--- src/Game/Util/CameraUpdate.cpp
+++ src/Game/Util/CameraUpdate.cpp
@@ -6,45 +6,42 @@
 #include <Component/Lens.hpp>
 #include <Util/Log.hpp>
 #include <Component/Physics.hpp>
+#include <limits.h>
 
 void UpdateCamera(Entity* camera, const std::vector<Entity*>& players){
     int numberOfPlayers = players.size();
-    float cameraX, cameraZ, cameraY;
-    glm::vec2 min, max;
-    cameraX = cameraZ = cameraY = 0.f;
-    //Find camera.
+    glm::vec3 cameraPos = glm::vec3(0.f, 0.f, 0.f);
+    glm::vec3 min = glm::vec3(std::numeric_limits<float>::max(), 0.f, std::numeric_limits<float>::max());
+    glm::vec3 max = glm::vec3(-std::numeric_limits<float>::max(), 0.f, -std::numeric_limits<float>::max());
     
+    //Find camera.  
     float heightFactor = 3.f;
     float widthFactor = 2.f;
 
     for (int i = 0; i < numberOfPlayers; i++) {
-        float x, z;
-        x = players[i]->GetComponent<Component::Transform>()->position.x;
-        z = players[i]->GetComponent<Component::Transform>()->position.z;
+        glm::vec3 playerPos = players[i]->GetComponent<Component::Transform>()->position;
 
         //Find min/max player positions
-        if (x*widthFactor > max.x)
-            max.x = x*widthFactor;
-        else if (x*widthFactor < min.x)
-            min.x = x*widthFactor;
+        if (playerPos.x*widthFactor > max.x)
+            max.x = playerPos.x*widthFactor;
+        if (playerPos.x*widthFactor < min.x)
+            min.x = playerPos.x*widthFactor;
 
-        if ((z*heightFactor) > max.y)
-            max.y = z*heightFactor;
-        else if ((z*heightFactor) < min.y)
-            min.y = z*heightFactor;
+        if ((playerPos.z*heightFactor) > max.z)
+            max.z = playerPos.z*heightFactor;
+        if ((playerPos.z*heightFactor) < min.z)
+            min.z = playerPos.z*heightFactor;
 
-        cameraX += x;
-        cameraZ += z;
+        cameraPos.x += playerPos.x;
+        cameraPos.z += playerPos.z;
     }
     float playerFactor = 1.f / static_cast<float>(numberOfPlayers);
 
-    cameraX = cameraX * playerFactor;
-    cameraZ = cameraZ * playerFactor;
+    cameraPos.x *= playerFactor;
+    cameraPos.z *= playerFactor;
     float distance = glm::distance(min, max)*0.75f;
     distance = glm::clamp(distance, 30.f, 60.f);
-    cameraY = distance;
+    cameraPos.y = distance;
 
-    glm::vec3 endPosition = glm::vec3(cameraX,cameraY,cameraZ);
-
-    camera->GetComponent<Component::Transform>()->position = glm::vec3(cameraX, cameraY, cameraZ);
+    camera->GetComponent<Component::Transform>()->position = cameraPos;
 }
