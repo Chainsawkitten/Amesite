6a2350094576e1be659e2a61146c64708e9c8479
Albin Bernhardsson <catscratchesp@gmail.com>
2016-02-08 11:49:17 +0100

    Render to render target.

--- src/Engine/System/RenderSystem.cpp
+++ src/Engine/System/RenderSystem.cpp
@@ -18,6 +18,7 @@
 #include <string>
 
 #include "../Lighting/DeferredLighting.hpp"
+#include "../RenderTarget.hpp"
 
 using namespace System;
 
@@ -37,7 +38,7 @@ RenderSystem::~RenderSystem() {
     Resources().FreeShaderProgram(mShaderProgram);
 }
 
-void RenderSystem::Render(Scene& scene) {
+void RenderSystem::Render(Scene& scene, RenderTarget* renderTarget) {
     glm::vec2 screenSize = MainWindow::GetInstance()->GetSize();
     
     mDeferredLighting->SetTarget();
@@ -78,7 +79,8 @@ void RenderSystem::Render(Scene& scene) {
                 glDrawElements(GL_TRIANGLES, model->GetComponent<Component::Mesh>()->geometry->GetIndexCount(), GL_UNSIGNED_INT, (void*)0);
             }
         }
-        mDeferredLighting->ResetTarget();
+        
+        renderTarget->SetTarget();
         //mDeferredLighting->ShowTextures(screenSize);
         mDeferredLighting->Render(scene, camera, screenSize);
 
--- src/Engine/System/RenderSystem.hpp
+++ src/Engine/System/RenderSystem.hpp
@@ -7,6 +7,7 @@ class Scene;
 class Shader;
 class ShaderProgram;
 class DeferredLighting;
+class RenderTarget;
 
 namespace System {
     /// %System to render a scene.
@@ -21,8 +22,9 @@ namespace System {
             /// Render Scene containing entities.
             /**
              * @param scene Contains a bunch of entities.
+             * @param renderTarget Target to render to.
              */
-            void Render(Scene& scene);
+            void Render(Scene& scene, RenderTarget* renderTarget);
 
         private:
             Shader* mVertexShader;
