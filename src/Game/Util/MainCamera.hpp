874cd228af8936f026246ec898c72448cdb9ce2b
Albin Bernhardsson <catscratchesp@gmail.com>
2016-02-15 11:39:10 +0100

    Fix MainCamera.

--- src/Game/Util/MainCamera.hpp
+++ src/Game/Util/MainCamera.hpp
@@ -2,33 +2,33 @@
 
 class Entity;
 
+/// Singleton containing the main camera.
 class MainCamera {
     public:
-        ///Gets main camera instance.
+        /// Gets main camera instance.
         /**
-        * @return The MainCamera instance.
-        */
-        static MainCamera& MainCamera::GetInstance();
+         * @return The MainCamera instance.
+         */
+        static MainCamera& GetInstance();
 
-        ///Sets the main camera entity.
+        /// Sets the main camera entity.
         /**
-        * @param camera Pointer to the camera that is the main camera for the scene.
-        */
+         * @param camera Pointer to the camera that is the main camera for the scene.
+         */
         void SetMainCamera(Entity* camera);
 
-        ///Gets the main camera entity.
+        /// Gets the main camera entity.
         /**
-        * @return The main camera entity.
-        */
+         * @return The main camera entity.
+         */
         Entity& GetMainCamera();
 
     private:
-        ///Main camera entity.
         Entity* mCamera;
 };
 
 /// Gets the main camera.
 /**
-* @return The %MainCamera instance
-*/
+ * @return The %MainCamera instance
+ */
 MainCamera& MainCameraInstance();
