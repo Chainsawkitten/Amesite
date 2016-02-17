1ec783a41c3e2bea5d5b87b3256bba789d78e4f5
splusharn <splusharn@gmail.com>
2016-02-17 01:26:57 +0100

    Further work on marching square algorithm

--- src/Engine/Util/MarchingSquares.cpp
+++ src/Engine/Util/MarchingSquares.cpp
@@ -1,33 +1,81 @@
 #include "MarchingSquares.hpp"
 #include "../Util/Log.hpp"
 
-std::vector<glm::vec2> MarchingSquares::marchingSquares(const bool ** data)
+using namespace MarchingSquares;
+
+std::vector<glm::vec2> MarchingSquares::marchingSquares(const bool ** data, const float squareSize)
 {
     int rows = sizeof(data) / sizeof(data[0]);
     int columns = sizeof(data[0]) / sizeof(data[0][0]);
-    std::vector<glm::vec2> lines;
 
-    for (int i = 0; i < rows - 1; i++) {
-        for (int j = 0; j < columns - 1; j++) {
-            int collisionType = getType(glm::bvec4(data[i][j], data[i + 1][j], data[i][j + 1], data[i + 1][j + 1]));
-            lines.push_back(getLine(glm::vec2(i, j), glm::vec2(i, j), glm::vec2(i, j), glm::vec2(i, j),collisionType));
+    float mapWidth = columns * squareSize;
+    float mapHeight = rows * squareSize;
+
+    ControlNode** controlNodes = new ControlNode*[columns];
+    MSquare** mSquares = new MSquare*[columns-1];
+
+    for (int x = 0; x < columns; x++) {
+        controlNodes[x] = new ControlNode[rows];
+        for (int y = 0; y < rows; y++) {
+            glm::vec3 pos = glm::vec3(-mapWidth / 2.f + x * squareSize + squareSize / 2.f, 0.f, -mapHeight / 2.f + y * squareSize + squareSize / 2.f);
+            controlNodes[x][y] = createControlNode(pos, data[x][y], squareSize);
         }
     }
+
+    for (int i = 0; i < columns - 1; i++) {
+        mSquares[i] = new MSquare[rows - 1];
+        for (int j = 0; j < rows - 1; j++) {
+            mSquares[i][j] = createMSquare(controlNodes[i][j + 1], controlNodes[i + 1][j + 1], controlNodes[i + 1][j], controlNodes[i][j]);
+        }
+    }
+
+
 }
 
-int MarchingSquares::getType(const glm::bvec4& points)
+MSquare MarchingSquares::createMSquare(ControlNode topLeft, ControlNode topRight, ControlNode bottomRight, ControlNode bottomLeft)
 {
-    int type = 0;
-    if (points[0]) type += 1; //Up, left
-    if (points[1]) type += 2; //Up, right
-    if (points[2]) type += 4; //Down, left
-    if (points[3]) type += 8; //Down, right
-    return type;
+    MSquare square;
+
+    square.topLeft = topLeft;
+    square.topRight = topRight;
+    square.bottomRight = bottomRight;
+    square.bottomLeft = bottomLeft;
+
+    square.centerTop = topLeft.right;
+    square.centerRight = bottomRight.above;
+    square.centerBottom = bottomLeft.right;
+    square.centerLeft = bottomLeft.above;
+
+    if (topLeft.active)
+        square.type += 8;
+    if (topRight.active)
+        square.type += 4;
+    if (bottomRight.active)
+        square.type += 2;
+    if (bottomLeft.active)
+        square.type += 1;
+
+    return square;
 }
 
-glm::vec2 MarchingSquares::getLine(const glm::vec2 points[4], const int type)
+ControlNode MarchingSquares::createControlNode(const glm::vec3 position, const bool active, const float squareSize)
 {
-    switch (type) {
+    ControlNode node;
+    node.active = active;
+    node.above = createMeshNode(position + glm::vec3(0.f, 0.f, 1.f)* squareSize / 2.f);
+    node.right = createMeshNode(position + glm::vec3(1.f, 0.f, 0.f)* squareSize / 2.f);
+}
+
+MeshNode MarchingSquares::createMeshNode(const glm::vec3 position)
+{
+    MeshNode node;
+    node.position = position;
+    return node;
+}
+
+glm::vec2 MarchingSquares::getTriangle(MSquare square)
+{
+    switch (square.type) {
     case 0:
         //No line
         break;
--- src/Engine/Util/MarchingSquares.hpp
+++ src/Engine/Util/MarchingSquares.hpp
@@ -4,24 +4,52 @@
 #include <vector>
 
 namespace MarchingSquares {
-    /// Perform the marching squares algorith on a set of data.
+    /// The node that is used to create mesh.
+    struct MeshNode {
+        /// Position of node.
+        glm::vec3 position;
+        /// Index of node.
+        int vertexIndex;
+    };
+
+    /// Node with data from the cellular automata.
+    struct ControlNode {
+        /// In mesh or not.
+        bool active;
+        /// Corresponding mesh nodes.
+        MeshNode above, right;
+    };
+
+    /// Representation of a square in the algorithm
+    struct MSquare {
+        ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        MeshNode centerTop, centerRight, centerBottom, centerLeft;
+        int type;
+    };
+    /// Perform the marching squares algorithm on a set of data.
     /**
     * @param data to analyze.
     * @return vector contraining the isolines. 
     */
-    std::vector<glm::vec2> marchingSquares(const bool **data);
+    std::vector<glm::vec2> marchingSquares(const bool **data, const float squareSize);
 
-    /// Get the type of intersection in the square examined.
+    /// Create a marching square from a set of data.
     /**
      * @param points of the square.
      * @return type, index between 0-15.
      */
-    int getType(const glm::bvec4& points);
+    MSquare createMSquare(ControlNode topLeft, ControlNode topRight, ControlNode bottomRight, ControlNode bottomLeft);
+
+    ControlNode createControlNode(const glm::vec3 position, const bool active, const float squareSize);
+
+    MeshNode createMeshNode(const glm::vec3 position);
+
+
 
     /// Get the type of intersection in the square examined.
     /**
     * @param points of the square.
     * @return type, index between 0-15.
     */
-    glm::vec2 getLine(const glm::vec2 points[4], const int type);
+    glm::vec2 getTriangle(MSquare square);
 }
