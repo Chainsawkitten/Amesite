16942026ee664d723e7f7c279ea3bb03866141d1
NinjaDanz3r <danielpejcinovski@hotmail.com>
2016-02-22 11:55:30 +0100

    Added doxygen comments to cave.

--- src/Game/GameObject/Cave.hpp
+++ src/Game/GameObject/Cave.hpp
@@ -12,6 +12,12 @@ namespace GameObject {
             /// Create %Cave
             /**
              * @param scene Pointer to which Scene %Cave Enities' are contained.
+             * @param width Map width.
+             * @param height Map height.
+             * @param seed The seed used for random generation.
+             * @param percent The amount of walls to place initially.
+             * @param iterations How many iterations of smoothing we will do.
+             * @param threshold The minimum room size in tiles.
              */
             Cave(Scene* scene, int width, int height, int seed, int percent, int iterations, int threshold);
            
