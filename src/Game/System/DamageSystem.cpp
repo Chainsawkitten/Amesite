34b90c41b90acd444910789d6355805713b02346
Ivar Björling <ivarbjorling@hotmail.com>
2016-02-24 09:27:23 +0100

    removed some comments

--- src/Game/System/DamageSystem.cpp
+++ src/Game/System/DamageSystem.cpp
@@ -51,28 +51,4 @@ void DamageSystem::Update(Scene& scene) {
             }
         }
     }
-
-    //std::vector<Scene::Collision*>* collisionVector = scene.GetVector<Scene::Collision>();
-    //int numberOfCollisions = collisionVector->size();
-    //for (int i = 0; i < numberOfCollisions; i++) {                                                                      //Loop through collision vector
-    //    if((*collisionVector)[i]->entity->GetComponent<Component::Health>() != nullptr) {                               //Does the colliding entity have a health component?
-    //        int numberOfIntersections = (*collisionVector)[i]->intersect.size();
-    //        for (int j = 0; j < numberOfIntersections; j++) {
-    //            if ( (*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>() != nullptr) {               //Does the intersecting entities have a damage component?
-    //                if ((*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>()->faction != (*collisionVector)[i]->entity->GetComponent<Component::Health>()->faction) { //Does the damaging if entity doesn't belong to the same faction as the health entity.
-    //                    (*collisionVector)[i]->entity->GetComponent<Component::Health>()->health -= (*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>()->damageAmount;   //Reduce health by damage.
-    //                    if ((*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>()->removeOnImpact && (*collisionVector)[i]->intersect[j]->GetComponent<Component::Reflect>() == nullptr) {// Remove damage entity if it should be removed on impact
-    //                        Component::Explode* explodeComp = (*collisionVector)[i]->intersect[j]->GetComponent<Component::Explode>();
-    //                        if (explodeComp != nullptr)
-    //                            GameEntityCreator().CreateExplosion((*collisionVector)[i]->intersect[j]->GetComponent<Component::Transform>()->GetWorldPosition()+explodeComp->offset, explodeComp->lifeTime, explodeComp->size, explodeComp->particleTextureIndex); // Create Explosion
-    //                        if ((*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>()->entity->gameObject != nullptr) // Remove game object
-    //                            (*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>()->entity->gameObject->Clear();
-    //                        else
-    //                            (*collisionVector)[i]->intersect[j]->GetComponent<Component::Damage>()->entity->Clear();
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
 }
