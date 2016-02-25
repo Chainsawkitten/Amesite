b9675e135dd9e5a533e833eeb04e5b967b7b52f3
Albin Bernhardsson <catscratchesp@gmail.com>
2016-02-25 15:17:44 +0100

    Don't log baseline.

--- src/Engine/Texture/Texture2D.cpp
+++ src/Engine/Texture/Texture2D.cpp
@@ -126,7 +126,6 @@ Texture2D::Texture2D(Font* font, const char* text) {
     
     glViewport(0, 0, mWidth, mHeight);
     glClear(GL_COLOR_BUFFER_BIT);
-    Log() << "Baseline: " << font->GetBaseline() << "\n";
     font->RenderText(text, glm::vec2(0.f, -static_cast<float>(font->GetBaseline())), 10000.f, glm::vec2(static_cast<float>(mWidth), static_cast<float>(mHeight)));
     
     glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
