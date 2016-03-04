b9193c55319d3cd7f338bf6cf455bbcc4d8f2091
Albin Bernhardsson <catscratchesp@gmail.com>
2016-03-04 11:56:30 +0100

    Rename Terrain member variables to adhere to code standards.

--- src/Engine/Geometry/Terrain.hpp
+++ src/Engine/Geometry/Terrain.hpp
@@ -12,37 +12,37 @@ namespace Geometry {
              * @param filename Filename (relative or absolute) to height map texture.
              */
             Terrain(const char* filename);
-
+            
             ///TODO comment.
             Terrain(float** floatArray, int width, int height, glm::vec2 textureRepeat);
-    
+            
             /// Destructor
             ~Terrain();
-    
+            
             /// Get all the vertices.
             /**
              * @return Array of vertices
              */
             Vertex* GetVertices() const;
-    
+            
             /// Get the number of vertices.
             /**
              * @return The number of vertices
              */
             unsigned int GetVertexCount() const;
-    
+            
             /// Get all the vertex indices.
             /**
              * @return Array of vertex indices
              */
             unsigned int* GetIndices() const;
-    
+            
             /// Get the number of indicies.
             /**
              * @return The number of vertex indices.
              */
             unsigned int GetIndexCount() const;
-    
+            
             /// Gets the height of the terrain at a given location.
             /**
              * If the position is outside the terrain, 0.0 is returned.
@@ -51,7 +51,7 @@ namespace Geometry {
              * @return Height of the terrain (0.0 - 1.0).
              */
             float GetY(float x, float z) const;
-    
+            
             /// Gets the normal at given location
             /**
              * If the position is outside the terrain, 0.0, 0.0, 0.0 is returned.
@@ -60,14 +60,14 @@ namespace Geometry {
              * @return Normal at that point.
              */
             glm::vec3 GetNormal(float x, float z) const;
-
+            
             /// Get how many times the texture should repeat.
             /**
              * Default: (1.0, 1.0)
              * @return How many times the texture should repeat
              */
             glm::vec2 GetTextureRepeat() const;
-    
+            
             /// Set how many times the texture should repeat.
             /**
              * @param repeat How many times the texture should repeat.
@@ -80,19 +80,19 @@ namespace Geometry {
             void Filter3x3();
             float SampleHeight(int x, int y) const;
             void CalculateNormals();
-    
-            Vertex *vertexData;
-            unsigned int vertexNr;
-    
-            unsigned int* indexData;
-            unsigned int indexNr;
-    
-            float** heightMap;
-            glm::vec3** normals;
-            glm::vec3** tangents;
-            int width;
-            int height;
-    
-            glm::vec2 textureRepeat;
+            
+            Vertex* mVertexData;
+            unsigned int mVertexNr;
+            
+            unsigned int* mIndexData;
+            unsigned int mIndexNr;
+            
+            float** mHeightMap;
+            glm::vec3** mNormals;
+            glm::vec3** mTangents;
+            int mWidth;
+            int mHeight;
+            
+            glm::vec2 mTextureRepeat;
     };
 }
