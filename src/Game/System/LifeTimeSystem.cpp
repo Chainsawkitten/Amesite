b6b9e037e274292d9a4f870ffc9f269bc59b8917
Albin Bernhardsson <catscratchesp@gmail.com>
2016-03-06 01:05:43 +0100

    Parallelize life time system.

--- src/Game/System/LifeTimeSystem.cpp
+++ src/Game/System/LifeTimeSystem.cpp
@@ -9,6 +9,8 @@
 #include "../Component/Explode.hpp"
 #include "../Component/LifeTime.hpp"
 
+#include <Threading/Threading.hpp>
+
 using namespace System;
 
 LifeTimeSystem::LifeTimeSystem() {
@@ -18,13 +20,29 @@ LifeTimeSystem::~LifeTimeSystem() {
 }
 
 void LifeTimeSystem::Update(Scene& scene, float deltaTime) {
-    std::vector<Component::LifeTime*> lifeTimes = scene.GetAll<Component::LifeTime>();
-    for (auto& lifeTimeComponent : lifeTimes) {
-        lifeTimeComponent->lifeTime -= deltaTime;
-        if (lifeTimeComponent->lifeTime < 0.f)
-            if (lifeTimeComponent->entity->gameObject != nullptr)
-                lifeTimeComponent->entity->gameObject->Kill();
+    std::vector<Component::LifeTime*>& lifeTimes = scene.GetAll<Component::LifeTime>();
+    
+    unsigned int threads = Threading::GetParallelCount();
+    std::size_t length = lifeTimes.size() / threads;
+    for (unsigned int i=0; i < threads; ++i) {
+        std::size_t begin = i * length;
+        if (i == threads - 1)
+            length = lifeTimes.size() - begin;
+        
+        if (length > 0)
+            Threading::FrontEndJobs().Add(std::bind(&LifeTimeSystem::UpdatePart, this, std::ref(lifeTimes), deltaTime, begin, length));
+    }
+    
+    Threading::FrontEndJobs().Wait();
+}
+
+void LifeTimeSystem::UpdatePart(std::vector<Component::LifeTime*>& lifeTimes, float deltaTime, std::size_t begin, std::size_t length) {
+    for (std::size_t i=begin; i < begin+length; ++i) {
+        lifeTimes[i]->lifeTime -= deltaTime;
+        if (lifeTimes[i]->lifeTime < 0.f)
+            if (lifeTimes[i]->entity->gameObject != nullptr)
+                lifeTimes[i]->entity->gameObject->Kill();
             else
-                lifeTimeComponent->entity->Kill();
+                lifeTimes[i]->entity->Kill();
     }
 }
--- src/Game/System/LifeTimeSystem.hpp
+++ src/Game/System/LifeTimeSystem.hpp
@@ -1,22 +1,31 @@
 #pragma once
 
+#include <vector>
+
 class Scene;
+namespace Component {
+    class LifeTime;
+}
 
 namespace System {
     /// %System to update the life time of entities and remove them is there life time is out.
     class LifeTimeSystem {
-    public:
-        /// Create life time system.
-        LifeTimeSystem();
-        
-        /// Destructor.
-        ~LifeTimeSystem();
-        
-        /// Update Scene containing entities. Updates life time components.
-        /**
-         * @param scene The scene to update.
-         * @param deltaTime Time since last frame (in seconds).
-         */
-        void Update(Scene& scene, float deltaTime);
+        public:
+            /// Create life time system.
+            LifeTimeSystem();
+            
+            /// Destructor.
+            ~LifeTimeSystem();
+            
+            /// Update Scene containing entities. Updates life time components.
+            /**
+             * @param scene The scene to update.
+             * @param deltaTime Time since last frame (in seconds).
+             */
+            void Update(Scene& scene, float deltaTime);
+            
+        private:
+            // Update part of the lifetime vector.
+            void UpdatePart(std::vector<Component::LifeTime*>& lifeTimes, float deltaTime, std::size_t begin, std::size_t length);
     };
 }
