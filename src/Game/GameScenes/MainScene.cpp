8bf0859f85b37cf64b358748061120a18eee5dbe
Ivar Björling <ivarbjorling@hotmail.com>
2016-03-10 13:59:09 +0100

    removed directional light

--- src/Game/GameScenes/MainScene.cpp
+++ src/Game/GameScenes/MainScene.cpp
@@ -176,12 +176,12 @@ MainScene::MainScene() {
         mCheckpointSystem.AddPlayer(player);
     }
     
-    // Directional light.
-    Entity* dirLight = CreateEntity();
-    dirLight->AddComponent<Component::Transform>()->pitch = 90.f;
-    dirLight->AddComponent<Component::DirectionalLight>();
-    dirLight->GetComponent<Component::DirectionalLight>()->color = glm::vec3(0.0000001f, 0.0000001f, 0.0000001f);
-    dirLight->GetComponent<Component::DirectionalLight>()->ambientCoefficient = 0.005f;
+    //// Directional light.
+    //Entity* dirLight = CreateEntity();
+    //dirLight->AddComponent<Component::Transform>()->pitch = 90.f;
+    //dirLight->AddComponent<Component::DirectionalLight>();
+    //dirLight->GetComponent<Component::DirectionalLight>()->color = glm::vec3(0.0000001f, 0.0000001f, 0.0000001f);
+    //dirLight->GetComponent<Component::DirectionalLight>()->ambientCoefficient = 0.005f;
     
     mPostProcessing = new PostProcessing(MainWindow::GetInstance()->GetSize());
     mFxaaFilter = new FXAAFilter();
