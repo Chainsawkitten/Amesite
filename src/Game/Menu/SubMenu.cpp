b51d8005cd9e87e6968554fee8b65ceff2be0c4b
Albin Bernhardsson <catscratchesp@gmail.com>
2016-03-10 12:18:07 +0100

    Sub menu rendering.

--- src/Game/Menu/SubMenu.cpp
+++ src/Game/Menu/SubMenu.cpp
@@ -1,11 +1,45 @@
 #include "SubMenu.hpp"
 
 #include "MenuOption.hpp"
+#include <Resources.hpp>
+#include <Font/Font.hpp>
+
+#include <MainWindow.hpp>
 
 SubMenu::SubMenu() {
+    // Load font.
+    float fontHeight = glm::ceil(MainWindow::GetInstance()->GetSize().y * 0.07f);
+    mFont = Resources().CreateFontFromFile("Resources/ABeeZee.ttf", fontHeight);
+    mFont->SetColor(glm::vec3(1.f, 1.f, 1.f));
     
+    // Define menu options.
+    mMenuOptions.push_back(new MenuOption(mFont, "START GAME", glm::vec3(0.f, 1.5f, 0.f), glm::vec3(0.f, 0.f, 0.f), 1.f));
+    //mMenuOptions[0]->callback = std::bind(&Menu::StartGame, this);
+    mMenuOptions.push_back(new MenuOption(mFont, "OPTIONS", glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 0.f), 1.f));
+    mMenuOptions.push_back(new MenuOption(mFont, "QUIT", glm::vec3(0.f, -1.5f, 0.f), glm::vec3(0.f, 0.f, 0.f), 1.f));
+    //mMenuOptions[2]->callback = std::bind(&Menu::Quit, this);
+    mSelected = 0;
+    
+    const glm::vec2& screenSize = MainWindow::GetInstance()->GetSize();
+    glViewport(0, 0, static_cast<int>(screenSize.x), static_cast<int>(screenSize.y));
 }
 
 SubMenu::~SubMenu() {
+    Resources().FreeFont(mFont);
     
+    for (MenuOption* menuOption : mMenuOptions)
+        delete menuOption;
+}
+
+void SubMenu::RenderSelected() {
+    const glm::vec2& screenSize = MainWindow::GetInstance()->GetSize();
+    
+    mMenuOptions[mSelected]->RenderSelected(screenSize, mModelMatrix);
+}
+
+void SubMenu::RenderMenuOptions() {
+    const glm::vec2& screenSize = MainWindow::GetInstance()->GetSize();
+    
+    for (MenuOption* menuOption : mMenuOptions)
+        menuOption->Render(screenSize, mModelMatrix);
 }
--- src/Game/Menu/SubMenu.hpp
+++ src/Game/Menu/SubMenu.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <glm/glm.hpp>
 #include <vector>
 
 class MenuOption;
+class Font;
 
 /// A sub menu inside a menu.
 class SubMenu {
@@ -13,7 +15,20 @@ class SubMenu {
         /// Destructor.
         ~SubMenu();
         
+        /// Render selected menu option.
+        void RenderSelected();
+        
+        /// Render menu options.
+        void RenderMenuOptions();
+        
     private:
+        glm::vec3 mPosition;
+        glm::vec3 mRotation;
+        glm::mat4 mModelMatrix;
+        
+        // Text.
+        Font* mFont;
+        
         // Menu options.
         std::vector<MenuOption*> mMenuOptions;
         int mSelected;
