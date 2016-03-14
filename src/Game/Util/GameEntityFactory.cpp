9482b55c7d061b4b4536eed521b52f9a77cfa46d
Pontus Björkberg <pontus.bjorkberg@hotmail.com>
2016-03-14 10:00:24 +0100

    Added crash sites when killed

--- src/Game/Util/GameEntityFactory.cpp
+++ src/Game/Util/GameEntityFactory.cpp
@@ -176,7 +176,7 @@ Entity* GameEntityFactory::CreateCrashSite1() {
     crash->AddComponent<Component::Material>()->SetDiffuse("Resources/player1_body_diff_dead.png");
     crash->GetComponent<Component::Material>()->SetSpecular("Resources/player1_body_spec.png");
     crash->GetComponent<Component::Material>()->SetGlow("Resources/player1_body_glow.png");
-    crash->AddComponent<Component::Transform>()->scale *= 0.3f;
+    crash->AddComponent<Component::Transform>()->scale *= 0.25f;
 
     return crash;
 
@@ -186,10 +186,10 @@ Entity* GameEntityFactory::CreateCrashSite2() {
 
     Entity* crash = mScene->CreateEntity();
     crash->AddComponent<Component::Mesh>()->geometry = Resources().CreateOBJModel("Resources/player2_body.obj");
-    crash->AddComponent<Component::Material>()->SetDiffuse("Resources/player2_body_diff_dead.png");
-    crash->GetComponent<Component::Material>()->SetSpecular("Resources/player2_body_spec.png");
-    crash->GetComponent<Component::Material>()->SetGlow("Resources/player2_body_glow.png");
-    crash->AddComponent<Component::Transform>()->scale *= 0.3f;
+    crash->AddComponent<Component::Material>()->SetDiffuse("Resources/player2_diff_dead.png");
+    crash->GetComponent<Component::Material>()->SetSpecular("Resources/player2_spec.png");
+    crash->GetComponent<Component::Material>()->SetGlow("Resources/player2_glow.png");
+    crash->AddComponent<Component::Transform>()->scale *= 0.25f;
 
     return crash;
 
