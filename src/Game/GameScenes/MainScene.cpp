23318a770764f88590b890bbaf2d9afccd23263e
NinjaDanz3r <danielpejcinovski@hotmail.com>
2016-03-14 17:06:55 +0100

    Added height differences in heightmap.

--- src/Game/GameScenes/MainScene.cpp
+++ src/Game/GameScenes/MainScene.cpp
@@ -143,11 +143,11 @@ MainScene::MainScene() {
     mPortalPosition = glm::vec2(playerStartX, playerStartZ);
 
     // Create players 
-    Player1* player1 = GameEntityCreator().CreatePlayer1(glm::vec3(playerStartX + 1.f, 0.f, playerStartZ + 1.f));
+    //Player1* player1 = GameEntityCreator().CreatePlayer1(glm::vec3(playerStartX + 1.f, 0.f, playerStartZ + 1.f));
     Player2* player2 = GameEntityCreator().CreatePlayer2(glm::vec3(playerStartX - 1.f, 0.f, playerStartZ - 1.f));
-    mPlayers.push_back(player1);
+    //mPlayers.push_back(player1);
     mPlayers.push_back(player2);
-    HubInstance().mPlayers.push_back(player1);
+    //HubInstance().mPlayers.push_back(player1);
     HubInstance().mPlayers.push_back(player2);
 
     // Set menu position.
