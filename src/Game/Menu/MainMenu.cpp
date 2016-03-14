53d76a655272f407439835875b2261a27b78de1b
Albin Bernhardsson <catscratchesp@gmail.com>
2016-03-14 17:28:55 +0100

    Use larger font for titles.

--- src/Game/Menu/MainMenu.cpp
+++ src/Game/Menu/MainMenu.cpp
@@ -4,6 +4,9 @@
 #include <MainWindow.hpp>
 #include "Menu.hpp"
 
+#include <Resources.hpp>
+#include <Font/Font.hpp>
+
 MainMenu::MainMenu(Menu* parentMenu) : SubMenu() {
     // Define menu options.
     MenuOption* menuOption = new MenuOption(GetFont(), "START GAME", glm::vec3(0.f, 1.5f, 0.f), glm::vec3(0.f, 0.f, 0.f), 2.f);
@@ -16,7 +19,12 @@ MainMenu::MainMenu(Menu* parentMenu) : SubMenu() {
     menuOption->callback = std::bind(&MainMenu::Quit, this);
     AddMenuOption(menuOption);
     
-    SetTitleOption(new MenuOption(GetFont(), "MODERSHIP", glm::vec3(11.f, 1.f, 8.5f), glm::vec3(270.f, 0.f, 314.f), 6.f));
+    // Title.
+    float fontHeight = glm::ceil(MainWindow::GetInstance()->GetSize().y * 0.18f);
+    Font* font = Resources().CreateFontFromFile("Resources/ABeeZee.ttf", fontHeight);
+    font->SetColor(glm::vec3(1.f, 1.f, 1.f));
+    SetTitleOption(new MenuOption(font, "MODERSHIP", glm::vec3(11.f, 1.f, 8.5f), glm::vec3(270.f, 0.f, 314.f), 6.f));
+    Resources().FreeFont(font);
     
     const glm::vec2& screenSize = MainWindow::GetInstance()->GetSize();
     glViewport(0, 0, static_cast<int>(screenSize.x), static_cast<int>(screenSize.y));
--- src/Game/Menu/OptionsMenu.cpp
+++ src/Game/Menu/OptionsMenu.cpp
@@ -4,6 +4,9 @@
 #include <MainWindow.hpp>
 #include "Menu.hpp"
 
+#include <Resources.hpp>
+#include <Font/Font.hpp>
+
 OptionsMenu::OptionsMenu(Menu* parentMenu) : SubMenu() {
     // Define menu options.
     MenuOption* menuOption = new MenuOption(GetFont(), "SOME OPTION", glm::vec3(0.f, 1.5f, 0.f), glm::vec3(0.f, 0.f, 0.f), 0.35f);
@@ -14,7 +17,12 @@ OptionsMenu::OptionsMenu(Menu* parentMenu) : SubMenu() {
     menuOption->callback = std::bind(&Menu::Transition, parentMenu, 0);
     AddMenuOption(menuOption);
     
-    SetTitleOption(new MenuOption(GetFont(), "OPTIONS", glm::vec3(-3.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 90.f), 2.f));
+    // Title.
+    float fontHeight = glm::ceil(MainWindow::GetInstance()->GetSize().y * 0.18f);
+    Font* font = Resources().CreateFontFromFile("Resources/ABeeZee.ttf", fontHeight);
+    font->SetColor(glm::vec3(1.f, 1.f, 1.f));
+    SetTitleOption(new MenuOption(font, "OPTIONS", glm::vec3(-3.f, 0.f, 0.f), glm::vec3(0.f, 0.f, 90.f), 2.f));
+    Resources().FreeFont(font);
     
     const glm::vec2& screenSize = MainWindow::GetInstance()->GetSize();
     glViewport(0, 0, static_cast<int>(screenSize.x), static_cast<int>(screenSize.y));
