b611d2ba4a13beca4d251b4b3a5b8ba21c808167
NinjaDanz3r <danielpejcinovski@hotmail.com>
2016-03-15 13:08:24 +0100

    Commented on perlin noise class.

--- src/Game/Util/PerlinNoise.hpp
+++ src/Game/Util/PerlinNoise.hpp
@@ -1,14 +1,28 @@
 #pragma once
 #include <vector>
 
-class PerlinNoiseGenerator{
+///Perlin Noise Generator
+class PerlinNoiseGenerator {
 	public:
+        ///Constructor
+        /**
+         @param seed Seed to use for the generator.
+         */
 		PerlinNoiseGenerator(unsigned int seed);
 
+        ///Gets noise for given position.
+        /**
+         @param x X-position.
+         @param y Y-position.
+         @param z Z-position.
+         @return Value for given position.
+         */
 		float Noise(float x, float y, float z);
 	private:
 		float Fade(float t);
+
 		float Lerp(float t, float a, float b);
+
 		float Grad(int hash, float x, float y, float z);
 		std::vector<int> mPermutationVector;
 };
