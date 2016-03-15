92febd3c070219f89e72e30c40d400623c081ec5
Albin Bernhardsson <catscratchesp@gmail.com>
2016-03-15 14:09:03 +0100

    Fix warnings in Player2.

--- src/Game/GameObject/Player/Player2.cpp
+++ src/Game/GameObject/Player/Player2.cpp
@@ -495,7 +495,6 @@ void Player2::mUpdateFunction() {
     float rollFactor = 0.f;
     glm::vec3 frontDirection = mNode->GetComponent<Component::Transform>()->GetWorldDirection();
     glm::vec3 sideDirection = glm::cross(frontDirection, glm::vec3(0.f, 1.f, 0.f));
-    glm::vec3 upDirection = glm::cross(sideDirection, frontDirection);
     if (glm::length(velocity) > 0.01f) {
         pitchFactor = glm::dot(glm::normalize(velocity), frontDirection);
         rollFactor = glm::dot(glm::normalize(velocity), sideDirection);
