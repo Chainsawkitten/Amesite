11aeab4f735925b167c24c117ad0daacbf6dbf73
Albin Bernhardsson <catscratchesp@gmail.com>
2016-03-15 15:18:15 +0100

    Water texture repeat.

--- src/Engine/Water/Water.cpp
+++ src/Engine/Water/Water.cpp
@@ -7,10 +7,15 @@ Water::Water() {
     mWaterTexture = Resources().CreateTexture2DFromFile("Resources/Water.png");
     mDudvMap = Resources().CreateTexture2DFromFile("Resources/WaterDUDV.png");
     mNormalMap = Resources().CreateTexture2DFromFile("Resources/WaterNormal.png");
+    mTextureRepeat = glm::vec2(1.f, 1.f);
 }
 
 Water::~Water() {
     Resources().FreeTexture2D(mWaterTexture);
     Resources().FreeTexture2D(mDudvMap);
     Resources().FreeTexture2D(mNormalMap);
+}
+
+void Water::SetTextureRepeat(const glm::vec2& textureRepeat) {
+    mTextureRepeat = textureRepeat;
 }
--- src/Engine/Water/Water.hpp
+++ src/Engine/Water/Water.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 class Texture2D;
 
 /// Reflective water surface.
@@ -11,9 +13,16 @@ class Water {
         /// Destructor.
         ~Water();
         
+        /// Set how much the water textures should repat.
+        /**
+         * @param textureRepeat How many times the water textures should repeat.
+         */
+        void SetTextureRepeat(const glm::vec2& textureRepeat);
+        
     private:
         // Textures.
         Texture2D* mWaterTexture;
         Texture2D* mDudvMap;
         Texture2D* mNormalMap;
+        glm::vec2 mTextureRepeat;
 };
