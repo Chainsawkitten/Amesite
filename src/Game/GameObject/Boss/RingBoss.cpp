3bfd5b06c81acfd218d74c6a555f6b505dfb0882
splusharn <splusharn@gmail.com>
2016-03-16 15:28:58 +0100

    Explosion fixes

--- src/Game/GameObject/Boss/RingBoss.cpp
+++ src/Game/GameObject/Boss/RingBoss.cpp
@@ -23,7 +23,13 @@
 #include <Component/ParticleEmitter.hpp>
 #include <Component/Physics.hpp>
 
+#include "../Player/Player1.hpp"
+#include "../Player/Player2.hpp"
+#include "../Bullet.hpp"
+
+#include "../../Util/Hub.hpp"
 #include "../../Util/ControlSchemes.hpp"
+#include "../../Util/GameEntityFactory.hpp"
 
 using namespace GameObject;
 
@@ -38,7 +44,7 @@ RingBoss::RingBoss(Scene* scene) : SuperBoss(scene) {
     body->GetComponent<Component::Material>()->SetSpecular("Resources/pylon_spec.png");
     body->GetComponent<Component::Material>()->SetGlow("Resources/pylon_glow.png");
     body->AddComponent<Component::Collider2DCircle>()->radius = 6.f;
-    body->AddComponent<Component::Explode>()->size = 30.f;
+    body->AddComponent<Component::Explode>()->size = 120.f;
     body->GetComponent<Component::Explode>()->particleTextureIndex = Component::ParticleEmitter::PURPLE;
     body->GetComponent<Component::Explode>()->lifeTime = 1.5f;
     body->GetComponent<Component::Explode>()->offset.y = 5.0f;
