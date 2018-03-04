35bfcb5d3db15c7c8ab2022a5774c84bc45d8e64
Chainsawkitten <catscratchesp@gmail.com>
2018-03-04 11:44:39 +0100

    Create UV-sphere

--- src/Engine/System/DebugDrawingSystem.cpp
+++ src/Engine/System/DebugDrawingSystem.cpp
@@ -1,15 +1,28 @@
 #include "DebugDrawingSystem.hpp"
 
+#include <glm/gtc/constants.hpp>
+
+#define BUFFER_OFFSET(i) ((char *)nullptr + (i))
+
 using namespace System;
 
 DebugDrawingSystem* DebugDrawingSystem::mActiveInstance = nullptr;
 
 DebugDrawingSystem::DebugDrawingSystem() {
     mActiveInstance = this;
+    
+    // Create sphere vertex array.
+    glm::vec3* sphere;
+    CreateSphere(sphere, mSphereVertexCount, 14);
+    CreateVertexArray(sphere, mSphereVertexCount, mSphereVertexBuffer, mSphereVertexArray);
+    delete[] sphere;
 }
 
 DebugDrawingSystem::~DebugDrawingSystem() {
     mActiveInstance = nullptr;
+    
+    glDeleteBuffers(1, &mSphereVertexBuffer);
+    glDeleteVertexArrays(1, &mSphereVertexArray);
 }
 
 DebugDrawingSystem* DebugDrawingSystem::GetActiveInstance() {
@@ -36,5 +49,57 @@ void DebugDrawingSystem::Update(float deltaTime) {
             --i;
         } else
             mSpheres[i].duration -= deltaTime;
+    }
+}
+
+void DebugDrawingSystem::CreateVertexArray(const glm::vec3* positions, unsigned int positionCount, GLuint& vertexBuffer, GLuint& vertexArray) {
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, positionCount * sizeof(glm::vec3), positions, GL_STATIC_DRAW);
+    
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), BUFFER_OFFSET(0));
+    
+    glBindVertexArray(0);
+}
+
+// Create UV-sphere with given number of parallel and meridian lines.
+void DebugDrawingSystem::CreateSphere(glm::vec3*& positions, unsigned int& vertexCount, unsigned int detail) {
+    vertexCount = detail * (4 * detail - 2);
+    positions = new glm::vec3[vertexCount];
+    
+    // Horizontal lines (meridians).
+    unsigned int i = 0;
+    for (unsigned int m = 1; m < detail; ++m) {
+        float meridian = glm::pi<float>() * m / detail;
+        for (unsigned int p = 0; p <= detail; ++p) {
+            float parallel = 2.0f * glm::pi<float>() * p / detail;
+            float angle = glm::pi<float>() * 0.5f - meridian;
+            float y = sin(angle);
+            float x = cos(angle);
+            positions[i++] = glm::vec3(x * cos(parallel), y, x * sin(parallel));
+            if (p > 0 && p < detail)
+                positions[i++] = glm::vec3(x * cos(parallel), y, x * sin(parallel));
+        }
+    }
+    
+    // Vertical lines (parallels).
+    for (unsigned int p = 0; p < detail; ++p) {
+        float parallel = 2.0f * glm::pi<float>() * p / detail;
+        for (unsigned int m = 0; m <= detail; ++m) {
+            float meridian = glm::pi<float>() * m / detail;
+            float angle = glm::pi<float>() * 0.5f - meridian;
+            float y = sin(angle);
+            float x = cos(angle);
+            positions[i++] = glm::vec3(x * cos(parallel), y, x * sin(parallel));
+            if (m > 0 && m < detail)
+                positions[i++] = glm::vec3(x * cos(parallel), y, x * sin(parallel));
+        }
     }
 }
