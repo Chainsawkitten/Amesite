c590307150346829f9596fcb20dc4c16af9c3c13
Chainsawkitten <catscratchesp@gmail.com>
2018-03-11 19:00:45 +0100

    Normalize frustum clipping planes

--- src/Engine/Physics/Frustum.cpp
+++ src/Engine/Physics/Frustum.cpp
@@ -6,40 +6,28 @@
 namespace Physics {
     Frustum::Frustum(const glm::mat4& matrix) {
         // Left clipping plane
-        mPlanes[0].x = glm::row(matrix, 3).x + glm::row(matrix, 0).x;
-        mPlanes[0].y = glm::row(matrix, 3).y + glm::row(matrix, 0).y;
-        mPlanes[0].z = glm::row(matrix, 3).z + glm::row(matrix, 0).z;
-        mPlanes[0].w = glm::row(matrix, 3).w + glm::row(matrix, 0).w;
+        mPlanes[0] = glm::row(matrix, 3) + glm::row(matrix, 0);
+        mPlanes[0] /= glm::length(glm::vec3(mPlanes[0]));
     
         // Right clipping plane
-        mPlanes[1].x = glm::row(matrix, 3).x - glm::row(matrix, 0).x;
-        mPlanes[1].y = glm::row(matrix, 3).y - glm::row(matrix, 0).y;
-        mPlanes[1].z = glm::row(matrix, 3).z - glm::row(matrix, 0).z;
-        mPlanes[1].w = glm::row(matrix, 3).w - glm::row(matrix, 0).w;
+        mPlanes[1] = glm::row(matrix, 3) - glm::row(matrix, 0);
+        mPlanes[1] /= glm::length(glm::vec3(mPlanes[1]));
     
         // Top clipping plane
-        mPlanes[2].x = glm::row(matrix, 3).x - glm::row(matrix, 1).x;
-        mPlanes[2].y = glm::row(matrix, 3).y - glm::row(matrix, 1).y;
-        mPlanes[2].z = glm::row(matrix, 3).z - glm::row(matrix, 1).z;
-        mPlanes[2].w = glm::row(matrix, 3).w - glm::row(matrix, 1).w;
+        mPlanes[2] = glm::row(matrix, 3) - glm::row(matrix, 1);
+        mPlanes[2] /= glm::length(glm::vec3(mPlanes[2]));
     
         // Bottom clipping plane
-        mPlanes[3].x = glm::row(matrix, 3).x + glm::row(matrix, 1).x;
-        mPlanes[3].y = glm::row(matrix, 3).y + glm::row(matrix, 1).y;
-        mPlanes[3].z = glm::row(matrix, 3).z + glm::row(matrix, 1).z;
-        mPlanes[3].w = glm::row(matrix, 3).w + glm::row(matrix, 1).w;
+        mPlanes[3] = glm::row(matrix, 3) + glm::row(matrix, 1);
+        mPlanes[3] /= glm::length(glm::vec3(mPlanes[3]));
     
         // Near clipping plane
-        mPlanes[4].x = glm::row(matrix, 3).x + glm::row(matrix, 2).x;
-        mPlanes[4].y = glm::row(matrix, 3).y + glm::row(matrix, 2).y;
-        mPlanes[4].z = glm::row(matrix, 3).z + glm::row(matrix, 2).z;
-        mPlanes[4].w = glm::row(matrix, 3).w + glm::row(matrix, 2).w;
+        mPlanes[4] = glm::row(matrix, 3) + glm::row(matrix, 2);
+        mPlanes[4] /= glm::length(glm::vec3(mPlanes[4]));
     
         // Far clipping plane
-        mPlanes[5].x = glm::row(matrix, 3).x - glm::row(matrix, 2).x;
-        mPlanes[5].y = glm::row(matrix, 3).y - glm::row(matrix, 2).y;
-        mPlanes[5].z = glm::row(matrix, 3).z - glm::row(matrix, 2).z;
-        mPlanes[5].w = glm::row(matrix, 3).w - glm::row(matrix, 2).w;
+        mPlanes[5] = glm::row(matrix, 3) - glm::row(matrix, 2);
+        mPlanes[5] /= glm::length(glm::vec3(mPlanes[5]));
     }
     
     bool Frustum::Collide(const AxisAlignedBoundingBox& aabb) const {
